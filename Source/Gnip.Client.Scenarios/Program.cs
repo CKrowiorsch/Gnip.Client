80a7c78cf93388dbce6a8351d9db837f65b1b0fe
Christian Krowiorsch <krowiorsch@landaumedia.de>
2013-06-12 16:13:39 +0200

    setup nLog

--- Source/Gnip.Client.Scenarios/Program.cs
+++ Source/Gnip.Client.Scenarios/Program.cs
@@ -16,6 +16,8 @@ namespace Krowiorsch.Gnip
             const string FileToAccessToken = @"c:\Data\gnip_access.json";
             var accessToken = GnipAccessToken.FromJsonStream(new FileStream(FileToAccessToken, FileMode.Open));
 
+            Logger.Info(string.Format("Use AccessToken: Username:{0} Password:{1}", accessToken.Username, accessToken.Password));
+
             Console.ReadLine();
         }
     }
