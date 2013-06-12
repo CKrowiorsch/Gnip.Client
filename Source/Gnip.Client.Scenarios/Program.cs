7654102a918515d6ea22f588fc9269222620ce08
Christian Krowiorsch <krowiorsch@landaumedia.de>
2013-06-12 16:11:15 +0200

    integrate scenario project

---
+++ Source/Gnip.Client.Scenarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+using Krowiorsch.Gnip.Model;
+
+using NLog;
+
+namespace Krowiorsch.Gnip
+{
+    class Program
+    {
+        static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            const string FileToAccessToken = @"c:\Data\gnip_access.json";
+            var accessToken = GnipAccessToken.FromJsonStream(new FileStream(FileToAccessToken, FileMode.Open));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Source/Gnip.Client/Model/GnipAccessToken.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Krowiorsch.Gnip.Model
+{
+    public class GnipAccessToken
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public static GnipAccessToken FromJsonStream(Stream stream)
+        {
+            using (var reader = new StreamReader(stream, Encoding.Default))
+                return JsonConvert.DeserializeObject<GnipAccessToken>(reader.ReadToEnd());
+        }
+    }
+}
