3793f6fb38a2fea9492264b82df64dd61d79c999
Christian Krowiorsch <krowiorsch@landaumedia.de>
2014-11-06 14:41:21 +0100

    add Statistics to Author

--- Source/Gnip.Model/Data/EntryAuthor.cs
+++ Source/Gnip.Model/Data/EntryAuthor.cs
@@ -1,9 +1,29 @@
-﻿namespace Krowiorsch.Gnip.Model.Data
+﻿using System.Collections.Generic;
+
+namespace Krowiorsch.Gnip.Model.Data
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class EntryAuthor
     {
+        /// <summary> Name of the Author </summary>
         public string Name { get; set; }
 
+        /// <summary> Link zum Author </summary>
         public string Link { get; set; }
+
+        /// <summary> Statistics Informations </summary>
+        public IDictionary<string, long>  Statistics { get; set; }
+
+        /// <summary> bekannte Einträge für Autorhenstatistiken </summary>
+        public class KnownAuthorStatistics
+        {
+            /// <summary> Count of Following</summary>
+            public const string FollowingCount = "Following";
+
+            /// <summary> Count of Followers </summary>
+            public const string FollowedByCount = "FollowedBy";
+        }
     }
 }
