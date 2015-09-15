12b0d1a9bfaaea0fd3fb341679cb612f415f3a41
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2015-09-15 13:55:04 +0200

    inc version

--- Source/Gnip.Client/Properties/AssemblyInfo.cs
+++ Source/Gnip.Client/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("1e7fa821-ab8b-4125-8dc8-529bd0b4a90e")]
 
-[assembly: AssemblyVersion("0.6.9")]
-[assembly: AssemblyFileVersion("0.6.9")]
+[assembly: AssemblyVersion("0.6.10")]
+[assembly: AssemblyFileVersion("0.6.10")]
